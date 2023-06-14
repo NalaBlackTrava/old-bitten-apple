c7178b664eae683fbcf0c6d9b534a931635d5aad
Nala Ayaba <joaovictor070102@gmail.com>
2023-06-14 16:58:26 -0300

    Primeiro Commit

---
+++ index.js
@@ -0,0 +1,17 @@
+// Código JavaScript para adicionar comportamento aos botões
+
+var closeButton = document.querySelector('.close-button');
+var minimizeButton = document.querySelector('.minimize-button');
+var zoomButton = document.querySelector('.zoom-button');
+
+closeButton.addEventListener('click', function() {
+    // Lógica para fechar o programa
+});
+
+minimizeButton.addEventListener('click', function() {
+    // Lógica para minimizar o programa
+});
+
+zoomButton.addEventListener('click', function() {
+    // Lógica para maximizar o programa
+});
